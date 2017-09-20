ea7ea44002d2f18bbe4a16f22d316ec53e7ff1dc
Hector Lopez <hlj239@byu.net>
2017-09-20 14:33:58 -0600

    uploading beta spell corrector

--- Spell-Corrector/ISpellCorrector.java
+++ Spell-Corrector/ISpellCorrector.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 
 public interface ISpellCorrector {
 
+	@SuppressWarnings("serial")
+	public static class NoSimilarWordFoundException extends Exception{}
 	/**
 	 * Tells this <code>SpellCorrector</code> to use the given file as its dictionary
 	 * for generating suggestions.
@@ -11,13 +13,11 @@ public interface ISpellCorrector {
 	 * @throws IOException If the file cannot be read
 	 */
 	public void useDictionary(String dictionaryFileName) throws IOException;
-
 	/**
 	 * Suggest a word from the dictionary that most closely matches
 	 * <code>inputWord</code>
 	 * @param inputWord
 	 * @return The suggestion or null if there is no similar word in the dictionary
 	 */
-	public String suggestSimilarWord(String inputWord);
-
+	public String suggestSimilarWord(String inputWord)throws NoSimilarWordFoundException;
 }
--- Spell-Corrector/Main.java
+++ Spell-Corrector/Main.java
@@ -1,6 +1,7 @@
 package spell;
 
 import java.io.IOException;
+import spell.ISpellCorrector.NoSimilarWordFoundException;
 
 /**
  * A simple main class for running the spelling corrector. This class is not
@@ -12,7 +13,7 @@ public class Main {
 	 * Give the dictionary file name as the first argument and the word to correct
 	 * as the second argument.
 	 */
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws NoSimilarWordFoundException, IOException {
 		
 		String dictionaryFileName = args[0];
 		String inputWord = args[1];
@@ -20,7 +21,7 @@ public class Main {
 		/**
 		 * Create an instance of your corrector here
 		 */
-		ISpellCorrector corrector = null;
+		SpellCorrector corrector = new SpellCorrector();
 		
 		corrector.useDictionary(dictionaryFileName);
 		String suggestion = corrector.suggestSimilarWord(inputWord);
